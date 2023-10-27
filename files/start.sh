1532698ccc511ee95c78ecf952574b0a2eb59012
gust <138919371+gusts888@users.noreply.github.com>
2023-10-27 17:32:26 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #定义哪吒变量参数
-export NEZHA_SERVER="nz.f4i.cn:5555"
-export NEZHA_KEY="5ddVS93Eq0Uc9he880"
+export NEZHA_SERVER="tz.243619.xyz:20345"
+export NEZHA_KEY="zUyf1yXQuk68VR00D6"
 
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
